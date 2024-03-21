63c9d13653772606fd1461386fabb1d9f90b3d28
Tulza <40633228+tulza@users.noreply.github.com>
2024-03-22 11:27:59 +1300

    Updates

---
+++ assets/Button.stories-B_pcY4pq.js
@@ -0,0 +1,26 @@
+import{f as k}from"./index-xPMmI0Rf.js";import{B as z}from"./Button-AOFu-DEs.js";import"./jsx-runtime-cPtoOl5T.js";import"./index-G6kSzgdV.js";const E={title:"Example/Button",component:z,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:k()}},r={args:{primary:!0,label:"Button"}},e={args:{label:"Button"}},a={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}},s={args:{primary:!0,label:"Delete now",backgroundColor:"red"}};var t,n,c;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: "Button"
+  }
+}`,...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};var l,m,u;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    label: "Button"
+  }
+}`,...(u=(m=e.parameters)==null?void 0:m.docs)==null?void 0:u.source}}};var p,i,d;a.parameters={...a.parameters,docs:{...(p=a.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  args: {
+    size: "large",
+    label: "Button"
+  }
+}`,...(d=(i=a.parameters)==null?void 0:i.docs)==null?void 0:d.source}}};var g,b,B;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: "small",
+    label: "Button"
+  }
+}`,...(B=(b=o.parameters)==null?void 0:b.docs)==null?void 0:B.source}}};var y,S,f;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: "Delete now",
+    backgroundColor: "red"
+  }
+}`,...(f=(S=s.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};const L=["Primary","Secondary","Large","Small","Warning"];export{a as Large,r as Primary,e as Secondary,o as Small,s as Warning,L as __namedExportsOrder,E as default};
---
+++ assets/syntaxhighlighter-JOJW2KGS-_00eEJfh.js
@@ -0,0 +1,1 @@
+import{S as g,c as n,s as u,a as c}from"./index-FhPNs4Us.js";import"./iframe-Va2scdDS.js";import"../sb-preview/runtime.js";import"./index-G6kSzgdV.js";import"./index-UiEO8yJN.js";import"./index-ZKB_KljB.js";import"./index-xt_wVKZ3.js";import"./index-PPLHz8o0.js";export{g as SyntaxHighlighter,n as createCopyToClipboardFunction,u as default,c as supportedLanguages};
